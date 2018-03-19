401fff001c936aecd44fe2694e4088fb55b436d5
Paul Gorman <gorman.paul@gmail.com>
2018-03-18 22:29:11 -0400

    Just about useful.

---
+++ README.md
@@ -0,0 +1,60 @@
+Weemon
+========================================================================
+
+Weemon checks the availability of hosts.
+Weemon depends on the system `ping` command, and that the output of `ping` is unix-like:
+
+```
+ping -c 3 10.0.0.2
+PING 10.0.0.2 (10.0.0.2) 56(84) bytes of data.
+64 bytes from 10.0.0.2: icmp_seq=1 ttl=64 time=0.367 ms
+64 bytes from 10.0.0.2: icmp_seq=2 ttl=64 time=0.367 ms
+64 bytes from 10.0.0.2: icmp_seq=3 ttl=64 time=0.379 ms
+
+--- 10.0.0.2 ping statistics ---
+3 packets transmitted, 3 received, 0% packet loss, time 2049ms
+rtt min/avg/max/mdev = 0.367/0.371/0.379/0.005 ms
+```
+
+Weemon uses JSON for its configuration file, like:
+
+```
+{
+	"debug": false,
+	"web": {
+		"ip": "127.0.0.1",
+		"port": "9444"
+	},
+	"ping": {
+		"checkInterval": "60",
+		"historyLength": "100",
+		"packetCount": "5",
+		"packetInterval": "0.3",
+		"lossReportRE": "^\\d+ packets transmitted, (\\d+) .+ (\\d+)% packet loss.*",
+		"rttReportRE": "^r.+ (\\d+\\.\\d+)/(\\d+\\.\\d+)/(\\d+\\.\\d+)/(\\d+\\.\\d+) ms$"
+	},
+	"targets": [
+		{ "name": "gateway", "address": "10.0.0.1" },
+		{ "name": "DNS server", "address": "10.0.0.2" },
+		{ "name": "web", "address": "10.0.0.11" }
+	]
+}
+```
+
+Weemon has these command-line options:
+
+- `-d` turns on debugging console output.
+- `-c` specifies the configuration file.
+
+
+License (2-clause BSD)
+------------------------------------------------------------------------
+
+Copyright 2018 Paul Gorman
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ weemon.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strconv"
+)
+
+type target struct {
+	ID      string
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+
+type configuration struct {
+	Debug bool `json:"debug"`
+	Web   struct {
+		IP   string `json:"ip"`
+		Port string `json:"port"`
+	} `json:"web"`
+	Ping struct {
+		CheckInterval  string `json:"checkInterval"`
+		HistoryLength  string `json:"historyLength"`
+		PacketCount    string `json:"packetCount"`
+		PacketInterval string `json:"packetInterval"`
+		LossReportRE   string `json:"lossReportRE"`
+		RTTReportRE    string `json:"rttReportRE"`
+	} `json:"ping"`
+	Targets []target `json:"targets"`
+}
+
+type pingResponse struct {
+	exitStatus string
+	raw        []byte
+}
+
+func configure() configuration {
+	c := flag.String("c", "/etc/weemon.conf", "Full path to weemon configuration file.")
+	d := flag.Bool("d", false, "Turn on debuggin messages.")
+	flag.Parse()
+
+	conf := configuration{}
+	conf.Debug = false
+	conf.Web.IP = "127.0.0.1"
+	conf.Web.Port = "9444"
+	conf.Ping.CheckInterval = "60"
+	conf.Ping.HistoryLength = "100"
+	conf.Ping.PacketCount = "5"
+	conf.Ping.PacketInterval = "0.3"
+	conf.Ping.LossReportRE = `^\d+ packets transmitted, (\d+) .+ (\d+)% packet loss.*`
+	conf.Ping.RTTReportRE = `^r.+ (\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+) ms$`
+
+	f, err := os.Open(*c)
+	if err != nil {
+		log.Fatal("error opening config file: ", err)
+	}
+	defer f.Close()
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&conf)
+	if err != nil {
+		log.Fatal("error decodng config JSON: ", err)
+	}
+
+	for k, v := range conf.Targets {
+		conf.Targets[k].ID = targetID(v.Name)
+	}
+
+	if *d {
+		conf.Debug = *d
+		fmt.Println("Starting weemon with debugging.")
+		fmt.Println("Using configuration file", *c)
+		fmt.Printf("Monitoring %v targets.\n", len(conf.Targets))
+	}
+
+	return conf
+}
+
+func ping(t *target, conf *configuration) {
+	var err error
+	var r pingResponse
+	lr := regexp.MustCompile(conf.Ping.LossReportRE)
+	rr := regexp.MustCompile(conf.Ping.RTTReportRE)
+
+	r.raw, err = exec.Command("ping", "-c", conf.Ping.PacketCount, "-i", conf.Ping.PacketInterval, t.Address).Output()
+	if err != nil && conf.Debug {
+		log.Println("ping failed:", err)
+	}
+	//	fmt.Println(t.Address, t.Name, t.ID)
+	sp := bytes.Split(r.raw, []byte("\n"))
+	if len(sp) > 3 {
+		if m := lr.FindSubmatch(sp[len(sp)-3]); m != nil {
+			loss := string(m[2])
+			pkts := string(m[1])
+			if pkts != conf.Ping.PacketCount || loss != "0" {
+				fmt.Println(t.Name, string(sp[len(sp)-3]))
+			}
+		}
+		if m := rr.FindSubmatch(sp[len(sp)-2]); m != nil {
+			// min := string(m[1])
+			avg, err := strconv.ParseFloat(string(m[2]), 32)
+			if err != nil {
+				log.Println(err)
+			}
+			// max := string(m[3])
+			dev, err := strconv.ParseFloat(string(m[4]), 32)
+			if err != nil {
+				log.Println(err)
+			}
+			if dev > (avg * 0.3) {
+				fmt.Println(t.Name, string(sp[len(sp)-2]))
+			}
+		}
+	}
+}
+
+func targetID(s string) string {
+	return fmt.Sprintf("%08x", s)
+}
+
+func main() {
+	if runtime.GOOS != "linux" {
+		log.Println("linux-like platform expected but not detected")
+	}
+	conf := configure()
+	for _, t := range conf.Targets {
+		ping(&t, &conf)
+	}
+}
